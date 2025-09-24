7d8b253e98f0bee8c6000a37c9b5daf907160eaf
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-09-25 00:51:57 +0530

    create-workflow api

--- app/api/routes/workflow.py
+++ app/api/routes/workflow.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 
-from app.schemas.workflow import WorkflowOut
+from app.schemas.workflow import WorkflowOut, WorkflowBase
 from app.api.deps import get_db
 from app.crud import workflow as crud_workflow
 from app.core.security import get_current_user
 
 router = APIRouter()
 
+# Get workflow API 
 @router.get("/workflow", response_model=List[WorkflowOut])
 def read_workflows(
     skip: int = 0,
@@ -17,3 +18,15 @@ def read_workflows(
     user_id: str = Depends(get_current_user)
 ):
     return crud_workflow.get_user_workflows(db, user_id=user_id, skip=skip, limit=limit)
+
+# Create workflow API
+@router.post("/create-workflow", response_model=WorkflowOut)
+def create_workflow(workflow_in: WorkflowBase, db:Session = Depends(get_db), user_id: str = Depends(get_current_user)):
+    return crud_workflow.create_workflow(
+        db,
+        user_id=user_id,
+        title=workflow_in.title,
+        enabled=workflow_in.enabled,
+        nodes=workflow_in.nodes,
+        connections=workflow_in.connections
+    )
--- app/crud/workflow.py
+++ app/crud/workflow.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.workflow import Workflow
+from datetime import datetime
 
 def get_user_workflows(db: Session, user_id: str, skip: int = 0, limit: int = 100):
     return (
@@ -9,3 +10,18 @@ def get_user_workflows(db: Session, user_id: str, skip: int = 0, limit: int = 10
         .limit(limit)
         .all()
     )
+
+def create_workflow(db: Session, user_id: str, title: str, enabled: bool, nodes: dict, connections: dict):
+    new_workflow = Workflow(
+        user_id=user_id,
+        title=title,
+        enabled=enabled,
+        nodes=nodes,
+        connections=connections,
+        created_at=datetime.isoformat(),
+        updated_at=datetime.isoformat()
+    )
+    db.add(new_workflow)
+    db.commit()
+    db.refresh(new_workflow)
+    return new_workflow
